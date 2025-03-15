396c4b16a2112fcf31d71ad8326f84a422be3451
pinkxz <victor666157@gmail.com>
2025-03-15 17:30:43 -0300

    comentários para organização

--- Ide/script.js
+++ Ide/script.js
@@ -1,66 +1,79 @@
+//Inicializando o editor de código usando CodeMirror, definindo alguns parâmetros
 const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
-    mode: "python",
-    lineNumbers: true,
-    theme: "default", 
-    indentUnit: 4, 
-    indentWithTabs: true, 
-  });
-  
-  // Configurando o botão RUN/STOP
-  const runButton = document.getElementById("run");
-  const consoleOutput = document.getElementById("console");
-  let worker;
-  
-  function startWorker(code) {
-    if (typeof Worker !== "undefined") {
-      if (worker === undefined) {
-        worker = new Worker("worker.js");
+  mode: "python",       
+  lineNumbers: true,    
+  theme: "default",      
+  indentUnit: 4,         
+  indentWithTabs: true,  
+});
+
+//Seleciona o botão de execução e a área de saída do console
+const runButton = document.getElementById("run");
+const consoleOutput = document.getElementById("console");
+
+//Variável do WebWorker
+let worker; 
+
+//Função para iniciar um Web Worker e executar código Python. Verifica se o navegador suporta e cria um caso não exista previamente
+function startWorker(code) {
+  if (typeof Worker !== "undefined") { 
+      if (worker === undefined) { 
+          worker = new Worker("worker.js");
       }
-  
+
+      //Definindo o que acontece ao receber mensagens do worker
       worker.onmessage = function (event) {
-        const { type, result } = event.data;
-        if (type === "output") {
-          appendOutput(result);
-        } else if (type === "error") {
-          appendError(result);
-        }
+          const { type, result } = event.data;
+          if (type === "output") {
+              appendOutput(result); 
+          } else if (type === "error") {
+              appendError(result); 
+          }
       };
-  
+
+      //Envia o código Python para o worker executar
       worker.postMessage(code);
-    } else {
+  } else {
       appendError("Web Workers não suportados pelo navegador.");
-    }
   }
-  
-  function stopWorker() {
-    if (worker) {
-      worker.terminate();
-      worker = undefined;
-    }
+}
+
+//Função para parar o Web Worker
+function stopWorker() {
+  if (worker) {
+      worker.terminate(); 
+      worker = undefined; 
   }
-  
-  function appendOutput(text) {
-    consoleOutput.innerHTML += `<div style="color: #eee;">${text}</div>`;
-    consoleOutput.scrollTop = consoleOutput.scrollHeight; // Rolagem automática para o final
-  }
-  
-  function appendError(text) {
-    consoleOutput.innerHTML += `<div style="color: red;">${text}</div>`;
-    consoleOutput.scrollTop = consoleOutput.scrollHeight; // Rolagem automática para o final
-  }
-  function clearConsole() {
-    consoleOutput.innerHTML = "";
-  }
-  // Evento para o botão RUN
-  runButton.addEventListener("click", () => {
-    if (runButton.textContent === "RUN") {
-      clearConsole()
-      runButton.textContent = "STOP";
-      startWorker(editor.getValue());
-    } else {
-      runButton.textContent = "RUN";
+}
+
+//Adiciona um texto ao console na cor padrao
+function appendOutput(text) {
+  consoleOutput.innerHTML += `<div style="color: #eee;">${text}</div>`;
+  consoleOutput.scrollTop = consoleOutput.scrollHeight;
+}
+
+//Adiciona um texto ao console na cor vermelha (para erros)
+function appendError(text) {
+  consoleOutput.innerHTML += `<div style="color: red;">${text}</div>`;
+  consoleOutput.scrollTop = consoleOutput.scrollHeight;
+}
+
+//Limpa a saída do console
+function clearConsole() {
+  consoleOutput.innerHTML = "";
+}
+
+//Evento de clique no botão RUN/STOP. 
+//Altera o botão RUN para STOP e vice-versa
+//Inicio o Worker com o código recebido do editor
+//Ou encerra.
+runButton.addEventListener("click", () => {
+  if (runButton.textContent === "RUN") {
+      clearConsole();
+      runButton.textContent = "STOP"; 
+      startWorker(editor.getValue()); 
+  } else {
+      runButton.textContent = "RUN"; 
       stopWorker();
-    }
-  });
-  
-  
+  }
+});
--- Ide/worker.js
+++ Ide/worker.js
@@ -1,24 +1,29 @@
+//Importando a biblioteca Pyodide
 importScripts('https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js');
 
+//Função assíncrona para carregar o Pyodide e executar o código Python recebido
 async function loadPyodideAndRun(code) {
+  // arregando o Pyodide e definindo as funções de saída padrão (stdout e stderr, para envio da saída ou mensagem de erro do código Python de volta para a thread principal
   let pyodide = await loadPyodide({
     stdout: (text) => {
-      self.postMessage({ type: 'output', result: text })
-      return
+      self.postMessage({ type: 'output', result: text });
+      return;
     },
     stderr: (text) => {
-      self.postMessage({ type: 'error', result: text })
-      return
+      self.postMessage({ type: 'error', result: text });
+      return;
     }
   });
-  try {
-    pyodide.runPython(code);
 
+  //Executa o código Python recebido ou captura e envia mensagens de erro em caso de falha
+  try {  
+    pyodide.runPython(code);
   } catch (err) {
-    self.postMessage({ type: 'error', result: err.message });
+    self.postMessage({ type: 'erro', result: err.message });
   }
 }
 
+//Evento que escuta mensagens enviadas da thread principal e chama a função para carregar e executar o código Python recebido
 self.onmessage = async (event) => {
-  loadPyodideAndRun(event.data)
+  loadPyodideAndRun(event.data);
 };
